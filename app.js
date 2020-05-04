0f5ead6deab8c8d17713440faebd57ad3d90834e
guanxiaodan <GXD9983@163.com>
2020-05-04 16:19:48 +0800

    访问接口

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var http = require('http')
 
 var server = http.createServer(function(request, response){
-  console.log('有人访问了node服务器')
-  response.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
-  response.write('给你了')
-  response.end()
+  console.log('有人访问了node服务器',request.url)
+  if(request.url == '/blog/myInfo'){
+    response.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
+    response.write('给你了')
+    response.end()
+  }
+  if(request.url == '/blog/third'){
+    response.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
+    response.write('Post请求。node给你')
+    response.end()
+  }
+
 })
 
 server.listen(2243)
--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,26 @@
 var express = require('express')
+// var bodyParser = require('body-parser') 
 
 var server = express()
+// server.use(bodyParser.urlencoded({extended:false}))
+// server.use(bodyParser.json())
+// server.all('*', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+//   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+//   res.header("X-Powered-By",' 3.2.1')`
+//   if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
+//   else  next();
+// });
 
-server.get('/',(request, response)=>{
+server.get('/api/list',(request, response)=>{
+  console.log('有人访问/api/list接口')
   response.send('这个是express服务器返回的内容--GET请求')
 })
 
-server.post('/', (request, response)=>{
+//TODO： 浏览器请求不到，报404，但postman可以。不知道为什么
+server.post('/api/fourth', (request, response)=>{
+  console.log('有人访问/api/fourth接口', request.data)
   response.send('这个是express服务器返回的内容--POST请求')
 })
 
